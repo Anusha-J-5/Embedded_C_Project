4c260001b7817a9b8f366c6ca66d4202f5d2b2c1
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-30 23:07:21 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,53 +1,28 @@
-/**
- * @file project_main.c
- * @author Bharath.G ()
- * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
- * @version 0.1
- * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-#include "project_config.h"
+#include "Activity1.h"
+#include "Activity2.h"
+#include "Activity3.h"
+#include "Activity4.h"
 
-#include "user_utils.h"
-#include "blinky.h"
-
-/**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
-void peripheral_init(void)
-{
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
-}
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
-/**
- * @brief Main function where the code execution starts
- * 
- * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
- */
+char temp_data;
 int main(void)
 {
-	/* Initialize Peripherals */
+	
+    InitADC();
+    char temp_data;
+    uint16_t temp;
 	peripheral_init();
+    timer();
 
-	for(;;)
+	while (1)
 	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
+        change_led_state(PD0);
+		_delay_ms(1000);
+         temp=ReadADC(0);
+        _delay_ms(200);
+        temp_data = PWM(temp);
+        UARTWritechar(temp_data);
+        OCR0A=0;
+        _delay_ms(200);
 	}
 	return 0;
 }
