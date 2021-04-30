f67c6825e2ead8ebdd340a65a88fde78dede6f7f
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-30 22:05:44 +0530

    Update act1.h

--- inc/act1.h
+++ inc/act1.h
@@ -1,1 +1,11 @@
+#ifndef ACT1_H_
+#define ACT1_H_
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void peripheral_init(void);
+void change_led_state(uint8_t state);
+
+#endif // ACT1_H_INCLUDED
 
