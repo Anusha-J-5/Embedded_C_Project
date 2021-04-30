45571adaaff0d0917ab2bf346f666e605c079a1e
Anusha-J-5 <81564900+Anusha-J-5@users.noreply.github.com>
2021-04-30 22:53:27 +0530

    Update act2.h

--- inc/act2.h
+++ inc/act2.h
@@ -1,1 +1,12 @@
+#ifndef ACT2_H_INCLUDED
+#define ACT2_H_INCLUDED
 
+#include <avr/io.h>
+#include<util/delay.h>
+#include <avr/interrupt.h>
+
+void InitADC();
+
+uint16_t ReadADC(uint8_t ch);
+
+#endif // ACT2_H_INCLUDED
